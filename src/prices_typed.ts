fd743072966e8a8b1f323a280a9b7f615241e81f
rush <rush@wusaby.com>
2024-04-30 14:39:38 +0300

    tcr: tests pass

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -87,6 +87,7 @@ function createApp(database: Database) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday = new Date(row.holiday);
+      let holiday2 = Temporal.PlainDate.from(row.holiday)
       if (
         date &&
         date.getFullYear() === holiday.getFullYear() &&
