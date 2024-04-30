8bc512856c2b228df4156a5fa569b303bf188e9c
rush <rush@wusaby.com>
2024-04-30 14:07:20 +0300

    tcr: tests pass

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -20,6 +20,7 @@ function createApp(database: Database) {
     const type = req.query.type as string;
     const baseCost = database.findBasePriceByType(type)!.cost;
     const date = parseDate(req.query.date as string);
+    const date2 = parseDate(req.query.date as string);
     const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
