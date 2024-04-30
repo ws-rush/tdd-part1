ae5052f7ab09f6ccb2b8e00faf663bf3c7d95589
rush <rush@wusaby.com>
2024-04-30 14:27:02 +0300

    tcr: tests pass

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -55,7 +55,7 @@ function createApp(database: Database) {
   }
 
   function calculateCostForDayTicket(age: number | undefined, date: Date | undefined, baseCost: number, date2: Temporal.PlainDate | undefined) {
-    let reduction = calculateReduction(date);
+    let reduction = calculateReduction(date, date2);
     if (age === undefined) {
       return Math.ceil(baseCost * (1 - reduction / 100));
     }
@@ -71,7 +71,7 @@ function createApp(database: Database) {
     return Math.ceil(baseCost * (1 - reduction / 100));
   }
 
-  function calculateReduction(date: Date | undefined) {
+  function calculateReduction(date: Date | undefined, date2: Temporal.PlainDate | undefined) {
     let reduction = 0;
     if (date && isMonday(date) && !isHoliday(date)) {
       reduction = 35;
