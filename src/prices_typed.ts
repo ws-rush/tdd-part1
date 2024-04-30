fd18fe70bf5ebf54a2461d2aa54a8e1343c1afcc
rush <rush@wusaby.com>
2024-04-30 14:52:37 +0300

    tcr: tests pass

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -33,11 +33,11 @@ function createApp(database: Database) {
   }
   function parsePlainDate(dateString: string | undefined): Temporal.PlainDate | undefined { if (dateString) { return Temporal.PlainDate.from(dateString) } }
 
-  function calculateCost(age: number | undefined, type: string, date2: Temporal.PlainDate | undefined, baseCost: number) {
+  function calculateCost(age: number | undefined, type: string, date: Temporal.PlainDate | undefined, baseCost: number) {
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);
     } else {
-      return calculateCostForDayTicket(age, date2, baseCost);
+      return calculateCostForDayTicket(age, date, baseCost);
     }
   }
 
